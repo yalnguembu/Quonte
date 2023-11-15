fae63486579b243edf435a0c31a4eef3eba95dc8
yalnguembu <yalnguembu@gmail.com>
2023-11-15 23:25:34 +0100

    task: update typescript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  test: {
-    coverage: {
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
-    },
-    globals: true,
   },
 });
